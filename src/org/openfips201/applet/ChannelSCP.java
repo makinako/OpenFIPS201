a534743545351a683d882c19c378d9b2fb5913ae
osully <34012554+osully@users.noreply.github.com>
2026-03-30 12:58:39 +1100

    Fixed max-length for R-ENC/R-MAC

--- src/org/openfips201/applet/ChannelSCP.java
+++ src/org/openfips201/applet/ChannelSCP.java
@@ -135,7 +135,7 @@ final class ChannelSCP {
 
     if ((scp.getSecurityLevel() & SecureChannel.R_ENCRYPTION) == SecureChannel.R_ENCRYPTION) {
       // R_ENCRYPTION + R_MAC is enabled
-      result = (short) 240; // 256 bytes - 8 byte R_MAC value = 248. Nearest multiple of 16 is 240. 
+      result = (short) 239; // 256 bytes - 8 byte R_MAC value = 248. Nearest multiple of 16 is 240. 
     } else if ((scp.getSecurityLevel() & SecureChannel.R_MAC) == SecureChannel.R_MAC) {
       // Only R_MAC is enabled
       result = (short) 248; // 256 bytes - 8 byte R_MAC value 
